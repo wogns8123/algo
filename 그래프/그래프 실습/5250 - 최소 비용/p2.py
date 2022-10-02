e66c63bdf72c119210c171817f65c474b72e5ab5
wogns8123 <wogns8123@naver.com>
2022-10-02 18:46:46 +0900

    algo

--- 그래프/그래프 실습/5250 - 최소 비용/p2.py
+++ 그래프/그래프 실습/5250 - 최소 비용/p2.py
@@ -1,31 +1,42 @@
 import heapq
-def f():
-    visited = [[100000 for _ in range(N)] for _ in range(N)]
-    visited[0][0] = 0
-    heap = []
-    heapq.heappush(heap,(0,0,0))
+import sys
+sys.stdin=open('input.txt')
+# 다익스트라:
+#   가장 누적 거리가 짧은 것을 반복해서 가면, 최소비용을 알 수 있다.
+#   가장 누적 거리가 짧은 것을 우선순위큐로 구현한다.
+# 우선순위큐:
+#   가장 우선순위가 높은 것을 큐에서 먼저 뺀다.  # heapq = 최소힙
 
-    while heap:
-        distance, x, y = heapq.heappop(heap)
-        if x == N-1 and y == N-1:
+def pdq():  # Priority Queue
+    visited = [[1000000 for _ in range(N)] for _ in range(N)]  # U
+    visited[0][0] = 0
+    hq = []  # heapq
+    heapq.heappush(hq, (0, 0, 0))  # 시작점
+    while hq:
+        pre_dis, x, y = heapq.heappop(hq)  # 현재누적거리, x, y
+        if x == N-1 and y == N-1:          # 마지막 노드에 도착했으면
             break
-        for rx,ry in ((x,y+1),(x+1,y),(x,y-1),(x-1,y)):
-            if 0<=rx<N and 0<=ry<N:
-                if arr[ry][rx] > arr[y][x]:
-                    distance = visited[y][x] + arr[ry][rx] - arr[y][x] + 1
+        for gx, gy in ((x, y+1), (x+1, y), (x, y-1), (x-1, y)):  # 델타탐색
+            if 0 <= gx < N and 0 <= gy < N:  # 그리드 안
+                # 거리구하기
+                if arr[gy][gx] > arr[y][x]:
+                    dis = visited[y][x] + arr[gy][gx] - arr[y][x] + 1
                 else:
-                    distance = visited[y][x] + 1
-                if distance < visited[ry][rx]:
-                    visited[ry][rx] = distance
-                    heapq.heappush(heap,(distance,rx,ry))
+                    dis = visited[y][x] + 1
+
+                # 거리가 더 가까우면, 덮어씌우기, 교재에서 D의 효과
+                if dis < visited[gy][gx]:
+                    visited[gy][gx] = dis  # 짧은거리를 덮어씌운다.
+                    heapq.heappush(hq, (dis, gx, gy))
     return visited[N-1][N-1]
 
 
 
-tc = int(input())
-for test in range(tc):
+TC = int(input())
+for tc in range(1, TC + 1):
     N = int(input())
-    arr = []
+    arr = list()
     for _ in range(N):
-        arr.append(list(map(int,input().split())))
-    print(f'#{test+1} {f()}')
+        arr.append(list(map(int, input().split())))
+
+    print(f'#{tc} {pdq()}')
